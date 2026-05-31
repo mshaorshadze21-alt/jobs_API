c9114b4936259857a7bb4ff122d7fe7e48705fe9
Mariam <mshaorshadze21@gmail.com>
2026-05-31 17:30:13 +0400

    vmushaob useEffect-is davalebaze (jobs-API)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Jobs from "./components/Jobs"
 function App() {
 
   const [data, setData] = useState(null)
+
+  const [selectedCard, setSelectedCard] = useState(null)
    
   useEffect(()=>{
     async function fetching (){
@@ -24,8 +26,39 @@ function App() {
   ,[])
   
   return (
-    <div>
-      {data && data.map((job, i)=><Jobs key={i} data={job}/>)}
+    <div className="m-10">
+      <ul className="grid grid-cols-3 gap-10">
+      {data && data.map((job, i)=>{
+        
+        /*<li><Jobs key={i} data={job}/></li>*/
+
+          if(selectedCard !== null && selectedCard !== i) {
+            return (null);
+          }
+              return (
+              <div className="flex flex-col gap-4">
+                <li key={i} onClick={() => setSelectedCard(i)}>
+                  <Jobs data={job} />
+                </li>
+
+                {selectedCard !==null && <button className="w-fit h-fit px-4 xy-2 bg-linear-to-br from-blue-600 to-blue-100 font-bold text-md rounded-md"
+                onClick={()=>setSelectedCard(null)}
+                >
+                  GO BACK
+                </button>   }
+
+                
+
+              </div>
+            );
+          }
+        
+        
+        )}
+
+
+
+      </ul>
 
     </div>
   )
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,10 +1,34 @@
-const Jobs = ({data}) => {
+import { useState } from "react"
+
+const Jobs = ({data, selectedCard}) => {
+
+  const [showMore, setShowMore] = useState (false);
+
+  const shortDescription = data.description.length > 200 ? data.description.slice (0, 200) + "..." : data.description
 
   return (
-    <div>
-        <h1>{data.company_name}</h1>
-        <p>{data.created_at}</p>
-     <div dangerouslySetInnerHTML={{ __html: data.description }} />
+    <div className="bg-linear-to-br from-blue-600 to-blue-100 w-100 h-fit p-4 rounded-2xl flex flex-col items-start gap-2">
+        <h1 className="text-xl font-bold">{data.company_name}</h1>
+        <h2>{data.title}</h2>
+        <p>{data.remote? "Remote job" : "On-site job"}</p>
+        {/* <p className="font-sm text-gray-700">see the link below</p> */}
+        
+
+        <div className="w-full h-fit">
+          <p className="font-medium text-lg">Description</p>
+          <div dangerouslySetInnerHTML={{ __html: showMore? data.description : shortDescription }} />
+
+          <button onClick={()=> setShowMore(!showMore)}>
+            {showMore? "See Less" : "See More"}
+           
+          </button>
+        </div>
+        {/* {showMore && <a href="{data.url}"></a>} */}
+
+        <a href={data.url} target="_blank">Find Jobs in Germany on Arbeitnow</a>
+
+        {/* {selectedCard == null && setShowMore(false)}
+         */}
 
     </div>
   )
